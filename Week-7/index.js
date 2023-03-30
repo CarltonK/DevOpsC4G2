eec1065fcc5a81576bd36bfe1e8df1ca7a26c4c7
Mark Carlton <mcarlton33@gmail.com>
2023-03-30 10:14:15 +0300

    week 7 docker and docker compose

--- Week-7/index.js
+++ Week-7/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -6,11 +7,30 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+const blogRoutes = require('./routes/blog');
+
+// Connect app to MongoDB
+const connectionString = process.env.DATABASE_URL || '';
+mongoose.connect(connectionString);
+
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log('Database connected');
+});
+
 app.get('/', (req, res) => {
     console.log('New request');
     res.status(200).json({ status: true });
 });
 
+// Routes
+app.use('/blog', blogRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
