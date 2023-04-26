26da335e6527c823941c64fbf2f3b8a55089e390
Mark Carlton <mcarlton33@gmail.com>
2023-04-26 09:29:59 +0300

    test coverage

---
+++ Week-8/test.js
@@ -0,0 +1,22 @@
+const server = require('./index');
+const request = require('supertest');
+
+describe('Base Endpoints', () => {
+    // Individual tests
+    it('Can do a basic GET request', async () => {
+        const response = await request(server)
+            .get('/')
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual({ running: true });
+    });
+    it('Can do a basic GET request on /users', async () => {
+        const response = await request(server)
+            .get('/user')
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual({ users: [] });
+    });
+});
+
+afterAll(() => {
+    server.close();
+});
