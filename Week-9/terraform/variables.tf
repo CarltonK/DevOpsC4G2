a551e3b4479230560a44f704a9f83cc9a33d0423
Mark Carlton <mcarlton33@gmail.com>
2023-05-25 10:12:52 +0300

    google and github providers

--- Week-9/terraform/variables.tf
+++ Week-9/terraform/variables.tf
@@ -6,3 +6,27 @@ variable "docker_images" {
     "hawksdev/g2-yolo-backend:1.0.0",
   ]
 }
+
+variable "instances" {
+  type = list(string)
+  default = [
+    "ansible-server",
+    "ansible-client-one",
+    "ansible-client-two",
+  ]
+}
+
+variable "token" {
+  type    = string
+  default = ""
+}
+
+variable "project_id" {
+  type    = string
+  default = ""
+}
+
+variable "organization_developers" {
+  type    = list(string)
+  default = [""]
+}
